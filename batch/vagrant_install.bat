1851da5d9388b9b818fd9f89448fdaf512401673
Jesper Oskarsson <jesosk@gmail.com>
2016-09-14 18:18:28 +0200

    Changed cygwin download path to working directory

--- batch/vagrant_install.bat
+++ batch/vagrant_install.bat
@@ -3,7 +3,7 @@ REM URL to cygwin-setup
 set "cygwin_setup_url=https://cygwin.com/setup-x86.exe"
 
 REM Download cygwin-setup
-PowerShell -Command "Invoke-WebRequest $env:cygwin_setup_url -OutFile 'C:\\cygwin_setup.exe'"
+PowerShell -Command "Invoke-WebRequest $env:cygwin_setup_url -OutFile 'cygwin_setup.exe'"
 
 REM Perform install
-C:\cygwin_setup.exe -s http://ftp.acc.umu.se/mirror/cygwin/ -P python -P make -q -B
+cygwin_setup.exe -s http://ftp.acc.umu.se/mirror/cygwin/ -P python -P make -q -B
