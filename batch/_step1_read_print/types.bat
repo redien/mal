7dea6fa16e12f105c565d1b53480bbc3e60f2f5f
Jesper Oskarsson <jesosk@gmail.com>
2016-09-28 00:44:09 +0200

    Fixed primitive reader.

--- batch/_step1_read_print/types.bat
+++ batch/_step1_read_print/types.bat
@@ -1,8 +1,4 @@
 
-set "NIL="
-set "TRUE=t"
-set "FALSE=f"
-
 :NIL?
     IF "!%~2!"=="" (
         set "%~1=!TRUE!"
@@ -12,14 +8,13 @@ set "FALSE=f"
 EXIT /B 0
 
 :CONS
+    set /a "_list_counter+=1"
     set "_list_first_!_list_counter!=!%~2!"
     set "_list_rest_!_list_counter!=!%~3!"
-    set /a "_list_counter+=1"
     set "%~1=L!_list_counter!"
 EXIT /B 0
 
 :FIRST
-:: Need to figure out how to dereference these
     set "ref=_list_first_!%~2:~1,2147483647!"
     set "%1=!%ref%!"
 EXIT /B 0
--- batch/header.bat
+++ batch/header.bat
@@ -1,8 +1,21 @@
 @echo off
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
+:: While macros
+set BREAK=EXIT
+set WHILE=IF "%1"=="fi301kvnro2qa9vm2" (FOR /L %%? IN () DO IF
+set DO=(
+set END_WHILE=) ELSE %BREAK%) ELSE CMD /Q /C "%~F0" fi301kvnro2qa9vm2
+
+:: Magic number that says we're calling a loop
+IF "%1"=="fi301kvnro2qa9vm2" GOTO %2
+
+set "NIL="
+set "TRUE=t"
+set "FALSE=f"
+
 GOTO :START
 
 :ECHO
-    IF NOT "!%~1!"=="" echo !%~1!
+    IF NOT "!%1!"=="" echo !%1!
 EXIT /B 0
